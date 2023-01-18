d3da6156bffe916741a7fa10df24b0a02bbc43e0
MahdiShokoufi <mahdishokoufi@gmail.com>
2023-01-18 17:29:02 +0330

    Add isPathExist function

--- project.c
+++ project.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <io.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int parse(){
     return 1;
@@ -16,6 +17,24 @@ int isFileExist(char *pth){
     return 0;
 }
 
+int isPathExist(char *pth){
+    //pth: root/sth/A.txt
+    struct stat st;
+    int len = strlen(pth);
+    char cur[1000];
+    for (int i = 0; i < len; i ++){
+        cur[i] = pth[i];
+        cur[i + 1] = '\0';
+        if (cur[i] == '/'){
+            cur[i] = '\0';
+            if (stat(cur, &st) == -1)
+                return 0;
+            cur[i] = '/';
+        }
+    }
+    return 1; //path exists
+}
+
 int createFile(char *pth){
     if (isFileExist(pth))
         return -1;
@@ -26,14 +45,16 @@ int createFile(char *pth){
     for (int i = 0; i < len; i ++){
         cur[i] = pth[i];
         cur[i + 1] = '\0';
-        if (cur[i] == '/'){
-            cur[i + 1] = '\0';
+        if (cur[i] == '/')
             mkdir(cur);
-        }
     }
     FILE *f = fopen(pth, "w");
     fclose(f);
     return 1; //file created
+}
+
+int insertString(char *pth, char *str, int line, int indx){
+    return 0;
 }
 
 int main(){
